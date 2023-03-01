0864b9653c9aabee516138c5d91d4185fd12ee8c
infovirus@ <game1201745@gmail.com>
2023-03-01 14:44:57 +0200

    To make a web-application

--- domain/Store/IbookRepository.cs
+++ domain/Store/IbookRepository.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Store
 {
-    interface IBookRepository
+    public interface IBookRepository
     {
         Book[] GetAllByTitle(string titlePart);
     }
---
+++ presentation/Store.Web/Models/ErrorViewModel.fs
@@ -0,0 +1,8 @@
+namespace Store.Web
+
+open System
+
+type ErrorViewModel private () =
+    member val RequestId : string = null with get, set
+
+    member val ShowRequestId : bool = true with get, set
